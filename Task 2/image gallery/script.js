8137eebd4adf445da37ad03cea915d263c6c4704
Ragavan-coder <ragavan.connect@gmail.com>
2025-11-23 14:45:03 +0530

    Initial commit

--- Task 2/image gallery/script.js
+++ Task 2/image gallery/script.js
@@ -5,33 +5,30 @@ const imgUrl = document.getElementById("imgUrl");
 const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightboxImg");
 
-// Extract real image URL from Google Image links
+
 function extractRealImageURL(url) {
   try {
     const urlObj = new URL(url);
-
-    // Google Image link usually contains the "imgurl" parameter
     const realImage = urlObj.searchParams.get("imgurl");
 
-    // If Google redirect found → return actual image URL
     if (realImage) return realImage;
 
-    // Otherwise return original
+    
     return url;
   } catch {
-    return url; // fallback for invalid URL
+    return url; 
   }
 }
 
-// ADD IMAGE
+
 addBtn.addEventListener("click", () => {
   let url = imgUrl.value.trim();
   if (!url) return;
 
-  // Fix Google redirect links
+  
   url = extractRealImageURL(url);
 
-  // Create gallery item
+  
   const div = document.createElement("div");
   div.className = "item";
 
@@ -45,14 +42,14 @@ addBtn.addEventListener("click", () => {
   imgUrl.value = "";
 });
 
-// REMOVE IMAGE
+
 gallery.addEventListener("click", (e) => {
   if (e.target.classList.contains("remove-btn")) {
     e.target.parentElement.remove();
   }
 });
 
-// OPEN LIGHTBOX
+
 gallery.addEventListener("click", (e) => {
   if (e.target.tagName === "IMG") {
     lightboxImg.src = e.target.src;
@@ -60,7 +57,7 @@ gallery.addEventListener("click", (e) => {
   }
 });
 
-// CLOSE LIGHTBOX
+
 lightbox.addEventListener("click", () => {
   lightbox.style.display = "none";
 });
--- Task 2/responsive webpage layout/script.js
+++ Task 2/responsive webpage layout/script.js
@@ -1,11 +1,10 @@
-// Mobile menu
+
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("navLinks");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
 
-// Scroll fade-in
 const fadeElements = document.querySelectorAll(".fade");
 
 const observer = new IntersectionObserver((entries) => {
@@ -16,7 +15,6 @@ const observer = new IntersectionObserver((entries) => {
 
 fadeElements.forEach(el => observer.observe(el));
 
-// Mouse-parallax movement for shapes
 document.addEventListener("mousemove", (e) => {
   const x = (window.innerWidth - e.pageX * 2) / 100;
   const y = (window.innerHeight - e.pageY * 2) / 100;
